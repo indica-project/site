aa67562a6b6854e456629459f3488069e780f65a
indica-project <153139216+indica-project@users.noreply.github.com>
2026-01-15 22:47:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 
-IMAGE_FOLDER = 'static\images'
+IMAGE_FOLDER = "static\images"
 
 @app.route('/')
 def index():
@@ -16,4 +16,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
